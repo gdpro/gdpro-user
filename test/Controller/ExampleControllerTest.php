2ecbdc750b3053ba0bf46a0f0cacd38d3044f93c
gary <gary.gitton@hotmail.fr>
2014-12-03 09:13:50 +0100

    create test config

---
+++ test/Controller/ExampleControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace GdproUserTest\UnitTest\Controller;
+
+use GdproUser\Controller\ExampleController;
+
+class ExampleControllerTest extends \PHPUnit_Framework_TestCase
+{
+    protected $controller;
+
+    protected $viewModelMock;
+
+    public function setUp()
+    {
+        $this->viewModelMock = $this->getMock('Zend\View\Model\ViewModel');
+
+        $this->controller = new ExampleController(
+            $this->viewModelMock
+        );
+    }
+
+    public function testIndexActionGood()
+    {
+        $result = $this->controller->indexAction();
+
+        $this->assertEquals($this->viewModelMock, $result);
+    }
+}
