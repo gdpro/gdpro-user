96172ae828f78029662851587c1b26cd9220f755
gary <gary.gitton@hotmail.fr>
2014-12-03 08:37:12 +0100

    remove submit button

--- src/Form/LoginForm.php
+++ src/Form/LoginForm.php
@@ -5,54 +5,40 @@ use Zend\Form\Form;
 
 class LoginForm extends Form
 {
-    public function __construct() {
-
-        parent::__construct('login');
-        $this->init();
-    }
-
-    public function init()
+    public function __construct()
     {
-        $this->add([
-                'name' => 'email',
-                'required' => true,
-                'options' => [
-                    'label' => 'Adresse email'
-                ],
-                'attributes' => [
-                    'type' => 'email'
-                ]
-            ]);
+        parent::__construct('login');
 
         $this->add([
-                'name' => 'password',
-                'options' => [
-                    'label' => 'Mot de passe'
-                ],
-                'attributes' => [
-                    'type' => 'password'
-                ]
-            ]);
+            'name' => 'email',
+            'required' => true,
+            'options' => [
+                'label' => 'Adresse email'
+            ],
+            'attributes' => [
+                'type' => 'email'
+            ]
+        ]);
 
         $this->add([
-                'name' => 'stayConnected',
-                'type' => 'Zend\Form\Element\Checkbox',
-                'options' => [
-                    'label' => 'Rester connecté',
-                    'use_hidden_element' => true,
-                    'checked_value' => 1,
-                    'unchecked_value' => 0
-                ]
-            ]);
+            'name' => 'password',
+            'options' => [
+                'label' => 'Mot de passe'
+            ],
+            'attributes' => [
+                'type' => 'password'
+            ]
+        ]);
 
         $this->add([
-                'name' => 'submit',
-                'attributes' => [
-                    'class' => 'btn btn-primary',
-                    'type' => 'submit',
-                    'value' => 'soumettre'
-                ],
-                'value' => 'submit'
-            ]);
+            'name' => 'stayConnected',
+            'type' => 'Zend\Form\Element\Checkbox',
+            'options' => [
+                'label' => 'Rester connecté',
+                'use_hidden_element' => true,
+                'checked_value' => 1,
+                'unchecked_value' => 0
+            ]
+        ]);
     }
 }
