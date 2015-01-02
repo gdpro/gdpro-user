bd2481254e18c6c9647350b792100d38707f69ed
gary <gary.gitton@hotmail.fr>
2015-01-02 11:10:09 +0100

    add method from userLogic

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -2,9 +2,25 @@
 namespace GdproUser\Repository;
 
 use Doctrine\ORM\Query\Expr;
-use GdproUser\Entity\UserAccount;
+use GdproUser\Entity\User;
 
-class UserRepository extends AbstractRepository
+class UserRepository extends EntityRepository
 {
+    /**
+     * Find One user by activationKey
+     *
+     * @param $activationKey
+     * @return mixed
+     */
+    public function findOneByActivationKey($activationKey)
+    {
+        $criteria = [
+            'activationKey' => $activationKey,
+            'deleted' => 0
+        ];
 
+        $user = $this->findOneBy($criteria);
+
+        return $user;
+    }
 }
